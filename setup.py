5ade44f702cf3615fd9fdd8de71d78c9595f3ef2
Abdul Ghafoor <aghafoor@Abduls-MacBook-Pro.local>
2017-06-02 00:47:58 +0100

    update setup.py with readme.md

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except ImportError:
 
 here = os.path.abspath(dirname(__file__))
 
-with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = '\n' + f.read()
 
 setup(
