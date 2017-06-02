790ca0050722b5fe38ad9798c8d390e714a5f226
Abdul Ghafoor <aghafoor@Abduls-MacBook-Pro.local>
2017-06-02 11:16:06 +0100

    Add setup files

--- setup.py
+++ setup.py
@@ -1,45 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-import sys
-import codecs
-
 from setuptools import setup
-
-try:
-    # Python 3
-    from os import dirname
-except ImportError:
-    # Python 2
-    from os.path import dirname
-
-here = os.path.abspath(dirname(__file__))
-
-with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = '\n' + f.read()
-
-setup(
-    name='whatep',
-    version='0.0.1',
-    description=' ',
-    long_description=long_description,
-    author='Abdul',
-    author_email='aghafoor@tssg.org',
-    url='https://github.com/agmahoon/whatep',
-    license='MIT',
-    classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-	'Operating System :: OS Independent',
-	'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-    ],
-)
-
+ 
+ setup(
+     name='my-awesome-helloworld-script',    # This is the name of your PyPI-package.
+     version='0.1',                          # Update the version number for new releases
+     scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
+ )
